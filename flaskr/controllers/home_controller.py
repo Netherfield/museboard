771259661047bfdefefef83f4155c634baa88ae3
PabloArt73 <159266946+PabloArt73@users.noreply.github.com>
2024-02-27 10:31:59 +0100

    27/02 10:31

--- flaskr/controllers/home_controller.py
+++ flaskr/controllers/home_controller.py
@@ -4,6 +4,12 @@ from flaskr.database.connection_manager import sqlite_connector, PATH_SQL
 
 home_blueprint = Blueprint("home", __name__)
 
+def get_branch():
+    """
+    SELECT FROM table first 4 branch
+    :return:
+    """
+    pass
 
 @home_blueprint.route("/home", methods= ["GET", "POST"])
 def home_control():
@@ -11,6 +17,7 @@ def home_control():
     return 'home_viewr' function from view and pass 'res' as argument
     :return:
     """
+    # res = get_branch()
     current_url = request.url
     print("home", current_url)
     res = [[1, ["topic1", "topic2", "topic3","topic4"]], [2, ["topic5","topic6","topic7","topic8"]],
--- flaskr/controllers/mydata_api.py
+++ flaskr/controllers/mydata_api.py
@@ -4,22 +4,33 @@ import random
 
 board_blueprint = Blueprint("board", __name__)
 
-@board_blueprint.route("/api/get_board")
-def board_control():
+
+def get_data():
+    """
+    Get Board Data -> ][1, [1,2,3,4]], [2, [1,2,3,4]], ...]
+    :return:
+    """
+    pass
+
+def url_parser():
     url = request.args.get('url')
     parsed_url = urlparse(url)
     params = parse_qs(parsed_url.query)
     tag = params.get('tag', [None])[0]
     path = params.get('path', [None])[0].split('/')
-    print('tag: ', tag, 'path: ', path[1:])
-    def get_data(arg=None):
-        """
-        Get Board Data -> ][1, [1,2,3,4]], [2, [1,2,3,4]], ...]
-        :return:
-        """
-        if arg:
-            # GET DATA
-            pass
+    return {'tag': tag, 'path': path[1:]}
+
+
+@board_blueprint.route("/api/get_board")
+def board_control():
+    url = url_parser()
+    tag = url['tag']
+    path = url['path']
+    """
+    SELECT FROM table WHERE tag or path..
+    """
+    print(tag, path)
+    # linzetta = get_data()
     linzetta = [10, []], [11, []], [12, []], [13, []]
     for x in range(len(linzetta)):
         for _ in range(4):
