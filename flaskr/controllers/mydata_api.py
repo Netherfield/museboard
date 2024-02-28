80a7a55313b69ef802c1a815293c6621f96531d5
Netherfield <barberisjacopo@gmail.com>
2024-02-28 11:43:49 +0100

    moved queries

--- flaskr/controllers/mydata_api.py
+++ flaskr/controllers/mydata_api.py
@@ -3,6 +3,8 @@ import random
 from urllib.parse import urlparse, parse_qs
 
 
+
+
 board_blueprint = Blueprint("board", __name__)
 
 def url_parser():
@@ -13,12 +15,32 @@ def url_parser():
     path = params.get('path', [None])[0].split('/')
     return {'tag': tag, 'path': path[1:]}
 
-def get_data():
+
+from queries.api import getBoards
+def get_data(branch:int, *args):
     """
-    Get Board Data -> ][1, [1,2,3,4]], [2, [1,2,3,4]], ...]
-    :return:
+    Retrieve data from branch by branch_id.
+    The return value is a list like so:
+    [ [('tag', 'branch'), ['item1', 'item2', ...], ... ]
+    Where branch is the branch_id relative to the tag, and items are the board items
     """
-    pass
+    default = {'random' : True, 'miss' : False}
+    boardData = getBoards(branch)
+    branchLookup = dict()
+    for line in boardData:
+        br, tag, item, item_id = int(line[2]), line[3], line[5], int(line[6])
+        try:
+            branchLookup[br]
+            branchLookup[br]['items'] += [item]
+        except:
+            branchLookup[br] = dict()
+            branchLookup[br]['tag'] = tag
+            branchLookup[br]['items'] = [item]
+    print(branchLookup)
+
+if __name__ == '__main__':
+    get_data(1)
+
 
 
 @board_blueprint.route("/api/get_board")
@@ -31,7 +53,7 @@ def board_control():
     SELECT FROM table WHERE tag or path..
     """
     print(tag, path)
-    # linzetta = get_data()
+    # linzetta = get_data(branch)
     linzetta = [10, []], [11, []], [12, []], [13, []]
     for x in range(len(linzetta)):
         for _ in range(4):
