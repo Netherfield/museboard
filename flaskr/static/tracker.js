e3be765dabda36d9e08783911d279e8507f3153c
PabloArt73 <159266946+PabloArt73@users.noreply.github.com>
2024-02-29 12:48:34 +0100

    Update tracker.js

--- flaskr/static/tracker.js
+++ flaskr/static/tracker.js
@@ -8,12 +8,15 @@ document.addEventListener('DOMContentLoaded', function() {
 
       // get ID from button
       var buttonId = this.getAttribute('data-id');
-
+      var divTag = this.parentElement.previousElementSibling;
+      console.log(divTag);
+      var buttonTag = divTag.innerHTML;
+      console.log(buttonTag);
       // get Text from button
       var buttonText = encodeURIComponent(this.textContent || this.innerText);
 
       // add to path
-      path += '/' + buttonText;
+      path += '/' + buttonTag;
 
       // reloading url in local (NO reload page)
       history.pushState(null, '', window.location.origin + window.location.pathname + '?item=' + buttonText + '&id=' + buttonId + '&path=' + path);
